083058beb66ab0e01cf7337918367e98943924d0
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-17 19:34:41 +0530

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "This is an App Center Post-Clone script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+apt-get update && apt-get install -y wget && rm -rf /var/lib/apt/lists/*
+wget -q https://download.qualibrate.com/qualibrate-cli
+chmod +x qualibrate-cli
+./qualibrate-cli help
