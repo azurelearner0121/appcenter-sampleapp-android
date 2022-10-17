1c8128fa277c87496a47fc101c62183640dc4f26
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-17 19:41:50 +0530

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -2,6 +2,6 @@
 
 #apt-get update && apt-get install -y wget && rm -rf /var/lib/apt/lists/*
 wget -q https://download.qualibrate.com/qualibrate-cli
-ls
 chmod +x qualibrate-cli
+ls -al
 ./qualibrate-cli help
