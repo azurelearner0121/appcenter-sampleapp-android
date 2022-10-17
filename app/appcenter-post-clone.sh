0db3620b2ca943d058899f0105712c53fdc1318f
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-17 22:58:10 +0530

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 docker pull nginx
-rm -rf /var/lib/apt/lists/*
-wget -q https://download.qualibrate.com/qualibrate-cli
-sudo chmod +x qualibrate-cli
-ls -al
-./qualibrate-cli --help
+
+docker run nginx
+#rm -rf /var/lib/apt/lists/*
+#wget -q https://download.qualibrate.com/qualibrate-cli
+#sudo chmod +x qualibrate-cli
+#ls -al
+#./qualibrate-cli --help
