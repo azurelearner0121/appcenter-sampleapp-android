643882051605978c561d134efffad3a37a03c1a6
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-19 18:38:23 +0530

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
-brew install --cask docker
-docker run nginx
-#rm -rf /var/lib/apt/lists/*
-#wget -q https://download.qualibrate.com/qualibrate-cli
-#sudo chmod +x qualibrate-cli
-#ls -al
-#./qualibrate-cli --help
+
+PROJECT_ID=12345
+SUITE_ID=12345
+CRT_API_URL="https://api.robotic.copado.com/pace/v4/projects/${PROJECT_ID}/jobs/${SUITE_ID}/builds"
+CRT_ACCESS_KEY=12345
+# Start the build
+BUILD=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' -d '{"inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${CRT_API_URL})
+echo "${BUILD}"
+BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
+if [ -z "${BUILD_ID}" ]; then
+  exit 1
+fi
+echo -n "Executing tests "
+STATUS='"executing"'
+# Poll every 10 seconds until the build is finished
+while [ "${STATUS}" == '"executing"' ]; do
+  sleep 10
+  RESULTS=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' ${CRT_API_URL}/${BUILD_ID})
+  STATUS=$(echo "${RESULTS}" | grep -Po '"status": *\K"[^"]*"' | head -1)
+  echo -n "."
+done
+echo " done!"
+FAILURES=$(echo ${RESULTS} | grep -Po '"failures":\K[0-9]+')
+LOG_REPORT_URL=$(echo "${RESULTS}" | grep -Po '"logReportUrl": *\K"[^"]*"')
+echo "Report URL: ${LOG_REPORT_URL}"
