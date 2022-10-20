29a91530f33df805abb2f74dae30013aa45325b4
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-20 11:28:47 +0530

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-echo "This is an App Center Pre-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+CRT_API_URL=https://api.robotic.copado.com/pace/v4/builds
+CRT_API_KEY="YRlFEdHaJR2oKEs0OVd7DuHJYmQG9DOq1vtz32WCglKlPwPPSuKF"
+# Start the build
+BUILD=$(curl -sS -d '{"key": "'${CRT_API_KEY}'", "inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${CRT_API_URL})
+echo "${BUILD}"
+BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
+if [ -z "${BUILD_ID}" ]; then
+  exit 1
+fi
+echo -n "Executing tests "
+STATUS='"executing"'
+# Poll every 10 seconds until the build is finished
+while [ "${STATUS}" == '"executing"' ]; do
+  sleep 10
+  RESULTS=$(curl -sS ${CRT_API_URL}/${BUILD_ID}?key=${CRT_API_KEY})
+  STATUS=$(echo "${RESULTS}" | grep -Po '"status": *\K"[^"]*"' | head -1)
+  echo -n "."
+done
+echo " done!"
+FAILURES=$(echo ${RESULTS} | grep -Po '"failures":\K[0-9]+')
+LOG_REPORT_URL=$(echo "${RESULTS}" | grep -Po '"logReportUrl": *\K"[^"]*"')
+echo "Report URL: ${LOG_REPORT_URL}"
