9d861f89abbf28ee1ac642137704f83a78777142
azurelearner0121 <107680151+azurelearner0121@users.noreply.github.com>
2022-10-26 14:59:40 +0530

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-PROJECT_ID=31567
-SUITE_ID=33274
-#CRT_API_URL="https://api.eu-robotic.copado.com/pace/v4/projects/${PROJECT_ID}/jobs/${SUITE_ID}/builds"
-#CRT_API_URL="https://api.eu-robotic.copado.com/pace.qentinel.com/robots/31567/r/20933/suite/33274"
-CRT_API_URL="https://api.eu-robotic.copado.com/pace/v4/projects/31567/jobs/33274/builds"
-echo
-CRT_ACCESS_KEY="YRlFEdHaJR2oKEs0OVd7DuHJYmQG9DOq1vtz32WCglKlPwPPSuKF"
-echo Start the build
-curl -v "${CRT_API_URL}"
-BUILD=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' -d '{"inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${CRT_API_URL})
-echo "${BUILD}"
-BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
-if [ -z "${BUILD_ID}" ]; then
-  exit 1
-fi
-echo -n "Executing tests "
-STATUS='"executing"'
+#PROJECT_ID=31567
+#SUITE_ID=33274
+##CRT_API_URL="https://api.eu-robotic.copado.com/pace/v4/projects/${PROJECT_ID}/jobs/${SUITE_ID}/builds"
+##CRT_API_URL="https://api.eu-robotic.copado.com/pace.qentinel.com/robots/31567/r/20933/suite/33274"
+#CRT_API_URL="https://api.eu-robotic.copado.com/pace/v4/projects/31567/jobs/33274/builds"
+#echo
+#CRT_ACCESS_KEY=""
+#echo Start the build
+#curl -v "${CRT_API_URL}"
+#BUILD=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' -d '{"inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${CRT_API_URL})
+#echo "${BUILD}"
+#BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
+#if [ -z "${BUILD_ID}" ]; then
+#  exit 1
+#fi
+#echo -n "Executing tests "
+#STATUS='"executing"'
 # Poll every 10 seconds until the build is finished
-while [ "${STATUS}" == '"executing"' ]; do
-  sleep 10
-  RESULTS=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' ${CRT_API_URL}/${BUILD_ID})
-  STATUS=$(echo "${RESULTS}" | grep -Po '"status": *\K"[^"]*"' | head -1)
-  echo -n "."
-done
-echo " done!"
-FAILURES=$(echo ${RESULTS} | grep -Po '"failures":\K[0-9]+')
-LOG_REPORT_URL=$(echo "${RESULTS}" | grep -Po '"logReportUrl": *\K"[^"]*"')
-echo "Report URL: ${LOG_REPORT_URL}"
+#while [ "${STATUS}" == '"executing"' ]; do
+#  sleep 10
+#  RESULTS=$(curl -sS -H 'X-Authorization: '"${CRT_ACCESS_KEY}"'' ${CRT_API_URL}/${BUILD_ID})
+#  STATUS=$(echo "${RESULTS}" | grep -Po '"status": *\K"[^"]*"' | head -1)
+#  echo -n "."
+#done
+#echo " done!"
+#FAILURES=$(echo ${RESULTS} | grep -Po '"failures":\K[0-9]+')
+#LOG_REPORT_URL=$(echo "${RESULTS}" | grep -Po '"logReportUrl": *\K"[^"]*"')
+#echo "Report URL: ${LOG_REPORT_URL}"
